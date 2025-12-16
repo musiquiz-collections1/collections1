d816067c83e857a1822af0f7a6e2906de8bbb7a8
musiquiz-collections1 <musiquiz-collections1@github.com>
2025-12-16 09:28:22 +0100

    Automated commit

--- build.py
+++ build.py
@@ -303,7 +303,7 @@ def update_index_html():
 		}}
 		#version > span {{
 			color: var(--color);
-			filter: brightness(10) contast(0.5);
+			filter: brightness(10) contrast(0.5);
 		}}
 	</style>
 </head>
